3f3cfd6941593ba2e66ce172f4c44272dc25a981
Jaehyun Kim <rlapo@naver.com>
2022-12-01 17:27:24 +0900

    dynamic allocation

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define MAXCONTACT 100
 
 typedef struct ContactInfo {
-	char name[21];
-	char phone[16];
-	char birth[9];
+	char* name;
+	char* phone;
+	char* birth;
 	struct ContactInfo* nxt; //다음 원소
 	struct ContactInfo* prv; //전 원소
 } Contact;
@@ -20,13 +19,24 @@ void findContactByBirth(Contact* head, int numContact);
 int main() {
 	Contact* head = NULL;
 	int numContact = 0;//연락처 개수
+	int mx;
+	//연결리스트로 연락처를 구현하면 이론상으로 구조체 포인터 배열을 사용하는 것 보다
+	//메모리 관리가 효율적이기에 구조체 포인터 배열을 사용하지 않고 1단계 때
+	//연결리스트로 구현했던 것을 유지했습니다 
+	//구조체 내부의 멤버 변수들은 동적할당했습니다
 	int menu;
+	printf("Max_num:");
+	scanf("%d", &mx);
 	while (1) {
 		printMainMenu(); //메뉴 출력
 		scanf("%d", &menu); //메뉴선택
 		switch (menu) {
 		case 1:
-			insertContact(&head, &numContact); //등록
+			if (numContact == mx) { //예외처리
+				printf("OVERFLOW\n");
+			}
+			else
+				insertContact(&head, &numContact); //등록
 			break;
 		case 2: printAll(head, numContact); // 보기
 			break;
@@ -35,8 +45,12 @@ int main() {
 		case 4: findContactByBirth(head, numContact); //생일자 검색
 			break;
 		case 5:
-			for (Contact* q = head; q != NULL; q = q->nxt)//해제
+			for (Contact* q = head; q != NULL; q = q->nxt) {//해제
+				free(q->name);
+				free(q->birth);
+				free(q->phone);
 				free(q);
+			}
 			return 0;
 		}
 	}
@@ -48,17 +62,20 @@ void printMainMenu() {
 }
 
 void insertContact(Contact** head, int* numContact) {
-	if (*numContact == MAXCONTACT) { //예외처리
-		printf("OVERFLOW\n");
-		return;
-	}
 	Contact* tmp = (Contact*)malloc(sizeof(Contact)); //할당
+	char t[100];
 	printf("Name:");
-	scanf("%s", tmp->name);
+	scanf("%s", t);
+	tmp->name = (char*)malloc(sizeof(char) * (strlen(t) + 1));
+	strcpy(tmp->name, t);
 	printf("Phone_number:");
-	scanf("%s", tmp->phone);
+	scanf("%s", t);
+	tmp->phone = (char*)malloc(sizeof(char) * (strlen(t) + 1));
+	strcpy(tmp->phone, t);
 	printf("Birth:");
-	scanf("%s", tmp->birth);
+	scanf("%s", t);
+	tmp->birth = (char*)malloc(sizeof(char) * (strlen(t) + 1));
+	strcpy(tmp->birth, t);
 	*numContact += 1;
 	sortContact(head, tmp); //함수 호출
 	printf("<<%d>>\n", *numContact);
@@ -104,7 +121,7 @@ void deleteContact(Contact** head, int* numContact) {
 		return;
 	}
 	*numContact -= 1;
-	char tmp[21];
+	char tmp[100];
 	printf("Name:");
 	scanf("%s", tmp);
 	Contact* q = *head;
@@ -125,8 +142,12 @@ void deleteContact(Contact** head, int* numContact) {
 			break;
 		}
 	}
-	if (q != NULL)//해제
+	if (q != NULL) {//해제
+		free(q->name);
+		free(q->birth);
+		free(q->phone);
 		free(q);
+	}
 	return;
 }
 
